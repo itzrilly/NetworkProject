16af71b537b90a24e11693daa576cae2f82b58e5
itzrilly <rilwanougarbarilly@gmail.com>
2022-07-23 19:26:57 +0100

    Initial Commit

---
+++ NetworkProject/NetworkProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkProjectApp.swift
+//  NetworkProject
+//
+//  Created by RILLY on 23/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct NetworkProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
